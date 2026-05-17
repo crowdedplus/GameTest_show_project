1482330ccbfe3ff6819bb284b9427ae3c8cfa00e
tyyyyy <1114964420@qq.com>
2026-05-17 19:49:20 +0800

    Make the video to be center in the line.

--- show_project/src/App.js
+++ show_project/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -8,11 +8,16 @@ function App() {
       {/* 方案一：B站嵌入，简洁参数 */}
       <iframe
         title="演示视频"
-        src="https://player.bilibili.com/player.html?bvid=BV1SURRB6ELi&page=1&autoplay=0&danmaku=0&high_quality=1"
+        src="https://player.bilibili.com/player.html?bvid=BV1SURRB6ELi&page=1&autoplay=0&danmaku=0&high_quality=1&as_wide=1"
         scrolling="no"
         frameBorder="0"
         allowFullScreen
-        style={{ width: '100%', height: '500px', border: 'none' }}
+        style={{ width: '60%',
+          height: '300px',
+          border: 'none', 
+          display: 'block',     
+          margin: '0 auto'      
+          }}
       />
     </div>
   );
