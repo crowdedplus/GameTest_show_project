f90c412748b74194356cbaf9bd582604f4f38606
tyyyyy <1114964420@qq.com>
2026-05-17 16:57:08 +0800

    Change default page into a video player of bilibili.

--- show_project/src/App.js
+++ show_project/src/App.js
@@ -3,21 +3,17 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ maxWidth: 960, margin: '0 auto', padding: '2rem' }}>
+      <h1>项目展示</h1>
+      {/* 方案一：B站嵌入，简洁参数 */}
+      <iframe
+        title="演示视频"
+        src="https://player.bilibili.com/player.html?bvid=BV1SURRB6ELi&page=1&autoplay=0&danmaku=0&high_quality=1"
+        scrolling="no"
+        frameBorder="0"
+        allowFullScreen
+        style={{ width: '100%', height: '500px', border: 'none' }}
+      />
     </div>
   );
 }
