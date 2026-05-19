1a2715d26170f87a80dd277a54ab84b6df81fc25
tyyyyy <1114964420@qq.com>
2026-05-19 17:12:56 +0800

    Added a release program component and its usage README.

--- show_project/src/App.js
+++ show_project/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div style={{ maxWidth: 960, margin: '0 auto', padding: '2rem' }}>
-      <h1>项目展示</h1>
+      <h1>哔哩哔哩外链播放器</h1>
       {/* 方案一：B站嵌入，简洁参数 */}
       <iframe
         title="演示视频"
@@ -12,13 +12,31 @@ function App() {
         scrolling="no"
         frameBorder="0"
         allowFullScreen
-        style={{ width: '60%',
+        style={{
+          width: '60%',
           height: '300px',
-          border: 'none', 
-          display: 'block',     
-          margin: '0 auto'      
-          }}
+          border: 'none',
+          display: 'block',
+          margin: '0 auto'
+        }}
       />
+      <h1>release方案</h1>
+      <video
+        controls
+        preload="auto"
+        style={{
+          width: '60%',
+          height: 'auto',
+          display: 'block',
+          margin: '0 auto'
+        }}
+      >
+        <source
+          src="https://github.com/crowdedplus/GameTest_show_project/releases/download/video_optimized/2-optimized.mp4"
+          type="video/mp4"
+        />
+        你的浏览器不支持视频播放。
+      </video>
     </div>
   );
 }
---
+++ show_project/src/instructions/README.md
@@ -0,0 +1,60 @@
+
+
+## Release方案托管视频
+
+
+
+**准备一个视频。**
+
+### **预处理视频**
+
+**在上传视频之前对视频进行预处理**，使用工具HandBrake.
+[HandBrake: Open Source Video Transcoder](https://handbrake.fr/)
+处理方法：
+
+#1 在概览 summary 界面，勾选网络视频优化选项。
+
+![image-20260519164329945](assets/image-20260519164329945.png)
+
+#2  在视频 Video 标签页，编码器选择 `H.264`，质量滑块往右拉一点（比如 `RF` 值 22-24）
+
+![image-20260519164712752](assets/image-20260519164712752.png)
+
+#3 点击开始编码 **Start Encode **生成MP4文件。
+
+![image-20260519164650619](assets/image-20260519164650619.png)
+
+**得到新的视频之后就可以准备上传了。**
+
+### **上传视频**
+
+直接对现有的release进行编辑即可。
+
+```bash
+# 标签[必选] 需要展示的项目名字，或者随便选一个，不影响使用。
+# 文件内容[必选] 上传新的视频即可。
+```
+
+**然后复制你要用的视频的链接，之后在页面中的使用方式：**
+
+```html
+<video
+  controls
+  preload="auto"
+  style={{
+  width: '60%',
+  height: 'auto',
+  display: 'block',
+  margin: '0 auto'
+  }}
+  >
+<source        src="https://github.com/用户名/仓库名/releases/download/video_optimized/2-optimized.mp4"
+   type="video/mp4"
+/>
+ 你的浏览器不支持视频播放。
+</video>
+```
+
+**`src` 的值就是下载视频的链接，在浏览器中直接右键复制链接即可。**
+
+![image-20260519170444836](assets/image-20260519170444836.png)
